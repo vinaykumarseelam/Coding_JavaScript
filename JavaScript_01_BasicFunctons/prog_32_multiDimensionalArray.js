a3acf78817059609848c021d9a07aa8dbf8476a3
admin <vinayseelam.98@gmail.com>
2022-06-13 11:57:38 -0700

    Multidimensinal array example

---
+++ JavaScript_01_BasicFunctons/prog_32_multiDimensionalArray.js
@@ -0,0 +1,13 @@
+var myArray = [[1,2,3],[4,5,6], [7,8,9], [[10,11,12],13,14]];
+
+var myData = myArray[0][2];
+console.log(myData);
+
+var myData1 = myArray[3][0];
+console.log(myData1);
+
+var myData2 = myArray[3][0][1];
+console.log(myData2);
+
+var myData3 = myArray[3][1];
+console.log(myData3);
