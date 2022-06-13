6a36b724cd6d4be9ed91e5a659d2fde4d666f417
admin <vinayseelam.98@gmail.com>
2022-06-13 11:22:38 -0700

    Finding last letter using bracket notation

---
+++ JavaScript_01_BasicFunctons/prog_31_lastletterusingBracketNotation.js
@@ -0,0 +1,12 @@
+var firstnameLength = 0;
+var firstName = "VinayKumarReddy";
+ firstnameLength = firstName.length;
+ console.log(firstnameLength);
+
+
+ let lastNameLength = 0;
+ let lastName = "Seelam";
+
+ lastNameLength = lastName.length;
+
+ console.log(lastName[lastNameLength-1]);
