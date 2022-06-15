dddc2fd104a410a272814ff86c0b7bb410f3a726
admin <admin@uscs-mac341.lan>
2022-06-15 09:46:53 -0700

    ifelse_Excercise

---
+++ JavaScript_Excercise/Excercise_03_ifelse.js
@@ -0,0 +1,9 @@
+var hour = 22;
+
+if (hour >= 6 && hour < 12) {
+  console.log("Good Morning !!");
+} else if (hour >= 12 && hour <= 18) {
+  console.log("Good afternoon");
+} else {
+  console.log("Good Evening");
+}
