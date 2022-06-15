eab0854a76ca8906dfddc4dcaad6d95bc9acb2f1
admin <admin@uscs-mac341.lan>
2022-06-14 23:43:51 -0700

    Added conditional Operators

--- JavaScript_01_BasicFunctons/prog_33_Operators.js
+++ JavaScript_01_BasicFunctons/prog_33_Operators.js
@@ -10,3 +10,18 @@ console.log(x / y);
 console.log(x++);
 console.log(x);
 console.log(++y);
+
+/// Comparison Operators
+
+//Relational Operators
+
+var v = 1;
+
+console.log(v >= 1);
+console.log(v > 1);
+console.log(v <= 1);
+console.log(v < 1);
+
+console.log(v === 1);
+
+console.log(v !== 1);
