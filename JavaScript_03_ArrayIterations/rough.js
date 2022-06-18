819843cbffce6d6d34675f5cf1f28a00a38ee2af
admin <admin@uscs-mac341.lan>
2022-06-18 12:20:31 -0700

    Grades

--- JavaScript_03_ArrayIterations/rough.js
+++ JavaScript_03_ArrayIterations/rough.js
@@ -1,21 +1,10 @@
-const strs = ["flower","flow","flight"];
-const newArray = [];
-    
-    var str1 = strs[0];
-    var str2 = strs[1];
-    var str3 = strs[2];
-    console.log(str1);
-    for(i=0; ((str1[i]===str2[i]) && (str1[i] === str3[i])); i++)
-        {
-            if((str1[i]===str2[i]) && (str1[i] === str3[i])){
-                newArray.push(str1);
-            }
-            else{
-                console.log(newArray.push(" "));
-            }
-        }
-    console.log(newArray);
+var array = 9;
 
-
-    
-    
+switch (array) {
+  case array > 1:
+    console.log("Lychee");
+    break;
+  case array > 5:
+    console.log("mango");
+    break;
+}
