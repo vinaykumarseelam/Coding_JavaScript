baf1da957c283a59ab58c356a5a2747f50cd112f
admin <admin@uscs-mac341.lan>
2022-06-27 13:55:01 -0700

    Added IIFE example

---
+++ JavaScript_06_iife/prog_01_iife_withParameters.js
@@ -0,0 +1,23 @@
+// A function which is immediatly defined after its defined is called IIFE.
+
+// A function declaration defines a function and does not require  a variable to be assigned to it.
+// It simply declares a function, and doesn't itself return a value.
+
+//(Immediatly invoked functoin expression)
+(function x(name) {
+  console.log(`say hi ${name}`);
+})("vinay Kumar Reddy");
+
+//Regular Method
+
+x = function (nationality) {
+  console.log(`My nationality is ${nationality}`);
+};
+
+x("Indian");
+
+// syntax : (function () {})()
+
+(function (nationality) {
+  console.log(`Hi I am from ${nationality}`);
+})("USA");
