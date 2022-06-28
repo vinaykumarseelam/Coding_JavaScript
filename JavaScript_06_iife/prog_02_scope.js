9cbe1c31d8687fdf3976749fc908d5653a7b2677
admin <admin@uscs-mac341.lan>
2022-06-28 10:07:52 -0700

    global, parent, child scope understanding

--- JavaScript_06_iife/prog_02_scope.js
+++ JavaScript_06_iife/prog_02_scope.js
@@ -7,3 +7,15 @@ function userName() {
 userName();
 
 console.log(username);
+
+//---------------------------------Scope-----------------
+var a = "I am global scope"; //global scope
+function simple() {
+  var a = "I am Parent Scope"; //parent scope
+  function c() {
+    //var a = "I am child Scope"; //child scope
+    console.log(a);
+  }
+  return c;
+}
+simple()();
