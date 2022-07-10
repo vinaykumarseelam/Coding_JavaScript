b4de18129121a424ab58ce3940a5b40f4423a17e
admin <admin@uscs-mac341.lan>
2022-07-10 10:53:32 -0700

    Added objects example

--- JavaScript_07_Objects/prog_02_constructors.js
+++ JavaScript_07_Objects/prog_02_constructors.js
@@ -1,1 +1,14 @@
 //--------------Constructors------------
+
+//-------------Objects Example -----------
+var obj = {
+  height: 3,
+  width: 7,
+  area: function () {
+    return this.width * this.height;
+  },
+};
+
+console.log(obj.area());
+obj.diameter = 22;
+console.log(obj);
